d8a80567dcab532c77813999697bb573f0777480
EC2 Default User <ec2-user@ip-172-31-26-41.eu-west-1.compute.internal>
2024-10-23 10:04:24 +0000

    “Fir”

--- demoproj/settings.py
+++ demoproj/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["130537cedd904893b48489ab30ab53fa.vfs.cloud9.eu-west-1.amazonaw
 
 INSTALLED_APPS = [
     'movies.apps.MoviesConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
